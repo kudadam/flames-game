498d37395182ffda8c232c8236e797a63e56898c
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-05-01 17:19:14 +0000

    trying deploy again

--- publish.js
+++ publish.js
@@ -3,6 +3,7 @@ import ghpages from "gh-pages";
 ghpages.publish(
     "docs",
     {
+        dest: "/docs",
         branch: "master",
         repo: "https://github.com/kudadam/flames-game.git",
         user: {
